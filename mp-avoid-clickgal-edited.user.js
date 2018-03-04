974bdc2d7e108d4273224a00c84510a7d3611967
Florian Leinsinger <leinzi1337@gmail.com>
2018-03-04 12:37:54 +0100

    Refactoring (in Funktion gekapselt)

--- mp-avoid-clickgal-edited.user.js
+++ mp-avoid-clickgal-edited.user.js
@@ -4,8 +4,7 @@
 // @description	  Bilderstrecken auf Moviepilot umgehen
 // @grant         none
 // @require				https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
-// @include       http://www.moviepilot.de/news/*
-// @include       https://www.moviepilot.de/news/*
+// @include       /^(https?:\/\/www\.moviepilot.de\/news\/)(.*?)$/
 // @version       1.07.1
 // ==/UserScript==
 
@@ -13,195 +12,62 @@
 //
 this.$ = this.jQuery = jQuery.noConflict(true);
 
+var regWithoutSuffix        = /^(https?:\/\/www\.moviepilot.de\/news\/)([^\/\#]*?)$/;
+var regFirstPageOne         = /^(https?:\/\/www\.moviepilot.de\/news\/)([^"]*?)\/(seite-1)$/;
+var regLatterPages          = /^(https?:\/\/www\.moviepilot.de\/news\/)([^"]*?)\/(seite-([2-9]|2[0-6]))$/;
+
+// gibt es nicht mehr?
+var regWithCommentSuffix    = /^(https?:\/\/www\.moviepilot.de\/news\/)([^"]*?)\#(comments)$/;
+
+// Funktion, damit das Dokument erst fertig geladen wird
+$(document).ready(function(){
   var isclicktrack = $('.js--article--click-track');
-  var GetURL = $(location).attr('href');
-  var EditURL = '';
-  var UseURL = '';
-//  var pageCount = 90;
+  var getURL = window.location.href.replace('.html', '');
 
-  js--article--click-track
-
-    console.log('Hallo');
   if (isclicktrack.length > 0) {
-    console.log('Hallo');
-    var lastPageURL = ('.js--pagination--last').attr('href');
+    var lastPageURL = $('.js--pagination--last').attr('href');
     var pieces = lastPageURL.split('-');
     var pageCount = Number(pieces[pieces.length-1].split('.')[0]);
-    console.log(pageCount);
-    if (/^(http:\/\/www\.moviepilot.de\/news\/)([^\/\#]*?)$/.test(window.location.href)) {
-      UseURL = GetURL + '/seite-';
-      $('.article--content-body').after('<p id=clickgal_cont_p></p>');
-      $('.article--content-body').after('<hr></hr>');
-      $('#clickgal_cont_p').append('<span id=clickgal_index><strong>Inhaltsverzeichnis:</strong></br>--------------------</span></br>');
-      $('#clickgal_cont_p').append('<span id=clickgal_content></span>');
-      $('#clickgal_cont_p').append('<span id=clickgal_content>--------------------</span></br>');
-      $('#clickgal_cont_p').append('<hr></hr>');
-      //    for (var i = 0; i < pageCount.length; i++) {
-      for (var i = 0; i < pageCount - 1; i++) {
-        $.ajax({
-          url: UseURL + (pageCount - i),
-          type: 'GET',
-          dataType: 'html',
-          async: false,
-          success: function (data) {
-            var html = $('<div>').html(data);
-            var Ergebnis1 = html.find('h1.article--header--title').html();
-            var Ergebnis2 = html.find('div.article--coverimage-caption').html();
-            if((pageCount - i - 1) % 10 == 0){
-              $('#clickgal_content').prepend('</br>');
-            }
-            $('#clickgal_content').prepend('<a href="' + this.url + '">' + this.url + '</a></br>');
-            $('#clickgal_content').prepend('<span id=clickgal_index_' + i + '>' + Ergebnis1 + ': </span></br>');
-          }
-        });
-      }
-    }
-    if (/^(http:\/\/www\.moviepilot.de\/news\/)([^"]*?)\/(seite-1)$/.test(window.location.href)) {
-      EditURL = GetURL.slice(0, - 1);
-      UseURL = EditURL;
-      $('.article--content-body').after('<p id=clickgal_cont_p></p>');
-      $('.article--content-body').after('<hr></hr>');
-      $('#clickgal_cont_p').append('<span id=clickgal_index><strong>Inhaltsverzeichnis:</strong></br>--------------------</span></br>');
-      $('#clickgal_cont_p').append('<span id=clickgal_content></span>');
-      $('#clickgal_cont_p').append('<span id=clickgal_content>--------------------</span></br>');
-      $('#clickgal_cont_p').append('<hr></hr>');
-      //    for (var i = 0; i < pageCount.length; i++) {
-      for (var i = 0; i < pageCount - 1; i++) {
-        $.ajax({
-          url: UseURL + (pageCount - i),
-          type: 'GET',
-          dataType: 'html',
-          async: false,
-          success: function (data) {
-            var html = $('<div>').html(data);
-            var Ergebnis1 = html.find('h1.article--header--title').html();
-            var Ergebnis2 = html.find('div.article--coverimage-caption').html();
-            if((pageCount - i - 1) % 10 == 0){
-              $('#clickgal_content').prepend('</br>');
-            }
-            $('#clickgal_content').prepend('<a href="' + this.url + '">' + this.url + '</a></br>');
-            $('#clickgal_content').prepend('<span id=clickgal_index_' + i + '>' + Ergebnis1 + ': </span></br>');
-          }
-        });
-      }
-    }
-    if (/^(http:\/\/www\.moviepilot.de\/news\/)([^"]*?)\/(seite-([2-9]|2[0-6]))$/.test(window.location.href)) {
-    }
-    if (/^(http:\/\/www\.moviepilot.de\/news\/)([^"]*?)\#(comments)$/.test(window.location.href)) {
-      EditURL = GetURL.slice(0, - 9);
-      UseURL = EditURL + '/seite-';
-      $('.article--content-body').after('<p id=clickgal_cont_p></p>');
-      $('.article--content-body').after('<hr></hr>');
-      $('#clickgal_cont_p').append('<span id=clickgal_index><strong>Inhaltsverzeichnis:</strong></br>--------------------</span></br>');
-      $('#clickgal_cont_p').append('<span id=clickgal_content></span>');
-      $('#clickgal_cont_p').append('<span id=clickgal_content>--------------------</span></br>');
-      $('#clickgal_cont_p').append('<hr></hr>');
-      //    for (var i = 0; i < pageCount.length; i++) {
-      for (var i = 0; i < pageCount - 1; i++) {
-        $.ajax({
-          url: UseURL + (pageCount - i),
-          type: 'GET',
-          dataType: 'html',
-          async: false,
-          success: function (data) {
-            var html = $('<div>').html(data);
-            var Ergebnis1 = html.find('h1.article--header--title').html();
-            var Ergebnis2 = html.find('div.article--coverimage-caption').html();
-            if((pageCount - i - 1) % 10 == 0){
-              $('#clickgal_content').prepend('</br>');
-            }
-            $('#clickgal_content').prepend('<a href="' + this.url + '">' + this.url + '</a></br>');
-            $('#clickgal_content').prepend('<span id=clickgal_index_' + i + '>' + Ergebnis1 + ': </span></br>');
-          }
-        });
-      }
-    }
-    if (/^(https:\/\/www\.moviepilot.de\/news\/)([^\/\#]*?)$/.test(window.location.href)) {
-      UseURL = GetURL + '/seite-';
-      $('.article--content-body').after('<p id=clickgal_cont_p></p>');
-      $('.article--content-body').after('<hr></hr>');
-      $('#clickgal_cont_p').append('<span id=clickgal_index><strong>Inhaltsverzeichnis:</strong></br>--------------------</span></br>');
-      $('#clickgal_cont_p').append('<span id=clickgal_content></span>');
-      $('#clickgal_cont_p').append('<span id=clickgal_content>--------------------</span></br>');
-      $('#clickgal_cont_p').append('<hr></hr>');
-      //    for (var i = 0; i < pageCount.length; i++) {
-      for (var i = 0; i < pageCount - 1; i++) {
-        $.ajax({
-          url: UseURL + (pageCount - i),
-          type: 'GET',
-          dataType: 'html',
-          async: false,
-          success: function (data) {
-            var html = $('<div>').html(data);
-            var Ergebnis1 = html.find('h1.article--header--title').html();
-            var Ergebnis2 = html.find('div.article--coverimage-caption').html();
-            if((pageCount - i - 1) % 10 == 0){
-              $('#clickgal_content').prepend('</br>');
-            }
-            $('#clickgal_content').prepend('<a href="' + this.url + '">' + this.url + '</a></br>');
-            $('#clickgal_content').prepend('<span id=clickgal_index_' + i + '>' + Ergebnis1 + ': </span></br>');
-          }
-        });
-      }
-    }
-    if (/^(https:\/\/www\.moviepilot.de\/news\/)([^"]*?)\/(seite-1)$/.test(window.location.href)) {
-      EditURL = GetURL.slice(0, - 1);
-      UseURL = EditURL;
-      $('.article--content-body').after('<p id=clickgal_cont_p></p>');
-      $('.article--content-body').after('<hr></hr>');
-      $('#clickgal_cont_p').append('<span id=clickgal_index><strong>Inhaltsverzeichnis:</strong></br>--------------------</span></br>');
-      $('#clickgal_cont_p').append('<span id=clickgal_content></span>');
-      $('#clickgal_cont_p').append('<span id=clickgal_content>--------------------</span></br>');
-      $('#clickgal_cont_p').append('<hr></hr>');
-      //    for (var i = 0; i < pageCount.length; i++) {
-      for (var i = 0; i < pageCount - 1; i++) {
-        $.ajax({
-          url: UseURL + (pageCount - i),
-          type: 'GET',
-          dataType: 'html',
-          async: true,
-          success: function (data) {
-            var html = $('<div>').html(data);
-            var Ergebnis1 = html.find('h1.article--header--title').html();
-            var Ergebnis2 = html.find('div.article--coverimage-caption').html();
-            if((pageCount - i - 1) % 10 == 0){
-              $('#clickgal_content').prepend('</br>');
-            }
-            $('#clickgal_content').prepend('<a href="' + this.url + '">' + this.url + '</a></br>');
-            $('#clickgal_content').prepend('<span id=clickgal_index_' + i + '>' + Ergebnis1 + ': </span></br>');
-          }
-        });
-      }
-    }
-    if (/^(https:\/\/www\.moviepilot.de\/news\/)([^"]*?)\/(seite-([2-9]|2[0-6]))$/.test(window.location.href)) {
-    }
-    if (/^(https:\/\/www\.moviepilot.de\/news\/)([^"]*?)\#(comments)$/.test(window.location.href)) {
-      EditURL = GetURL.slice(0, - 9);
-      UseURL = EditURL + '/seite-';
-      $('.article--content-body').after('<p id=clickgal_cont_p></p>');
-      $('.article--content-body').after('<hr></hr>');
-      $('#clickgal_cont_p').append('<span id=clickgal_index><strong>Inhaltsverzeichnis:</strong></br>--------------------</span></br>');
-      $('#clickgal_cont_p').append('<span id=clickgal_content></span>');
-      $('#clickgal_cont_p').append('<span id=clickgal_content>--------------------</span></br>');
-      $('#clickgal_cont_p').append('<hr></hr>');
-      //    for (var i = 0; i < pageCount.length; i++) {
-      for (var i = 0; i < pageCount - 1; i++) {
-        $.ajax({
-          url: UseURL + (pageCount - i),
-          type: 'GET',
-          dataType: 'html',
-          async: false,
-          success: function (data) {
-            var html = $('<div>').html(data);
-            var Ergebnis1 = html.find('h1.article--header--title').html();
-            var Ergebnis2 = html.find('div.article--coverimage-caption').html();
-            if((pageCount - i - 1) % 10 == 0){
-              $('#clickgal_content').prepend('</br>');
-            }
-            $('#clickgal_content').prepend('<a href="' + this.url + '">' + this.url + '</a></br>');
-            $('#clickgal_content').prepend('<span id=clickgal_index_' + i + '>' + Ergebnis1 + ': </span></br>');
-          }
-        });
-      }
+
+    if (regWithoutSuffix.test(getURL)) {
+      buildTableOfContents(getURL + '/seite-', pageCount);
+    } else if (regFirstPageOne.test(getURL)) {
+      buildTableOfContents(getURL.slice(0, - 1), pageCount);
+    } else if (regLatterPages.test(getURL)) {
+      // do nothing
+    } else if (regWithCommentSuffix.test(getURL)) {
+      buildTableOfContents(getURL.slice(0, - 9) + '/seite-', pageCount);
     }
   }
+});
+
+function buildTableOfContents(defURL, pageCount) {
+  var divider = '--------------------';
+  var contentBody = $('.article--content-body');
+  contentBody.after('<p id=clickgal_cont_p></p>');
+  contentBody.after('<hr>');
+
+  var clickGalContP = $('#clickgal_cont_p');
+  clickGalContP.append('<span id=clickgal_index><strong>Inhaltsverzeichnis:</strong></br>'+ divider +'</span></br>');
+  clickGalContP.append('<span id=clickgal_content></span>');
+  clickGalContP.append('<span id=clickgal_bottom>'+ divider +'</span></br>');
+  clickGalContP.append('<hr>');
+
+  for (var i = 1; i < pageCount - 1; i++) {
+    $.ajax({
+      url: defURL + (pageCount - i),
+      type: 'GET',
+      dataType: 'html',
+      async: false,
+      success: function (data) {
+        var html = $('<div>').html(data);
+        var header = html.find('h1.article--header--title').html();
+        if((pageCount - i - 1) % 10 == 0){
+          $('#clickgal_content').prepend('</br>');
+        }
+        $('#clickgal_content').prepend('<a href="' + this.url + '">' + this.url + '</a></br>');
+        $('#clickgal_content').prepend('<span id=clickgal_index_' + i + '>' + header + ': </span></br>');
+      }
+    });
+  }
+}
