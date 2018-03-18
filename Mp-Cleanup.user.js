1ab29013ffbbf1ba5c899dada3b5b45ea8714e56
Florian Leinsinger <leinzi1337@gmail.com>
2018-03-18 17:03:23 +0100

    Remove people page cleanup

--- Mp-Cleanup.user.js
+++ Mp-Cleanup.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name                MP-Cleanup (jQuery)
 // @description         Moviepilot generell bereinigen
+// @author              mitcharts, leinzi
 // @grant               none
 // @downloadURL         https://raw.githubusercontent.com/Leinzi/mp-Skripte/master/Mp-Cleanup.user.js
 // @require             https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @include             /^(https?:)\/\/(.+\.)?(moviepilot.de)\/(.*)$/
 // @exclude             /^(https?:)\/\/(.+\.)?(moviepilot.de)\/serie\/(.*)$/
-// @version             1.0.8
+// @version             1.1.0
 // ==/UserScript==
 
 // jQuery-Konflikte loesen
@@ -33,9 +34,6 @@ $(document).ready(function(){
 
   justifyTextContent();
 
-  if (/^(https?:\/\/www\.moviepilot.de\/people\/)([^\/\#]*?)$/.test(getURL)){
-    cleanUpActorPage();
-  }
 });
 
 function cleanUpMiddleBar(){
@@ -97,104 +95,4 @@ function justifyTextContent(){
   // Darstellerübersicht
   $('.person--description').css({'text-align': 'justify'});
 
-}
-
-function cleanUpActorPage() {
-  cleanUpEmptyParagraphs();
-  // Übersicht
-  // Blocksatz
-  $('.person--description').css({'text-align': 'justify'});
-  $('.comment--replies--list').css({'text-align': 'justify'});
-  $('.comment--body').css({'text-align': 'justify'});
-
-  //Rubriken verstecken
-  jQuery('.js--content-editor--sidebar').remove();
-  jQuery('#sidebar hr').remove();
-
-  var sidebar = jQuery('#sidebar');
-
-  var videoSection = document.createElement('section');
-  videoSection.setAttribute('class', 'sidebar-video');
-  sidebar.prepend(videoSection);
-  var videoHeader = jQuery('h4:contains("Videos zu")');
-  videoHeader.addClass('sidebar-video-header');
-  appendSelectionTo(videoSection, videoHeader);
-
-  var tvSection = document.createElement('section');
-  tvSection.setAttribute('class', 'sidebar-tv');
-  videoSection.after(tvSection);
-  var tvHeader = jQuery('h4:contains("im Fernsehen")');
-  tvHeader.addClass('sidebar-tv-header');
-  appendSelectionTo(tvSection, tvHeader);
-
-  var tvItems = jQuery('.consumption_item_tv');
-  appendSelectionTo(tvSection, tvItems);
-
-  var listSection = document.createElement('section');
-  listSection.setAttribute('class', 'sidebar-lists');
-  tvSection.after(listSection);
-  var listDiv = jQuery('div.page-updater.lists');
-  appendSelectionTo(listSection, listDiv);
-
-  var collabSection = document.createElement('section');
-  collabSection.setAttribute('class', 'sidebar-collab');
-  var collabHeader = jQuery('h4:contains("arbeitet oft zusammen mit")');
-  collabHeader.addClass('sidebar-collab-header');
-  var collabList = collabHeader.next('ul');
-  collabList.addClass('sidebar-collab-list');
-  appendSelectionTo(collabSection, collabHeader);
-  appendSelectionTo(collabSection, collabList);
-  listSection.after(collabSection);
-
-  var photoSection = document.createElement('section');
-  photoSection.setAttribute('class', 'sidebar-photos');
-  var photoHeader = jQuery('h4:contains("Bilder:")');
-  photoHeader.addClass('sidebar-photo-header');
-  var photoList = photoHeader.next('.movie-photos');
-  photoList.addClass('sidebar-photo-list');
-  var photoMore = photoList.next('.box-more');
-  photoMore.addClass('sidebar-photo-more');
-  appendSelectionTo(photoSection, photoHeader);
-  appendSelectionTo(photoSection, photoList);
-  appendSelectionTo(photoSection, photoMore);
-  collabSection.after(photoSection);
-
-  var fansSection = document.createElement('section');
-  fansSection.setAttribute('class', 'sidebar-fans');
-  var fansDiv = jQuery('#sidebar_followers');
-  appendSelectionTo(fansSection, fansDiv);
-  photoSection.after(fansSection);
-
-
-  var mainContent = jQuery('.js--content-editor');
-
-  var personSection = document.createElement('section');
-  personSection.setAttribute('class', 'main-person');
-  mainContent.prepend(personSection);
-  var personDetailsDiv = jQuery('.person--details');
-  var personDetailsMore = jQuery('.person--collapsible--more');
-  appendSelectionTo(personSection, personDetailsDiv);
-  appendSelectionTo(personSection, personDetailsMore);
-
-  //Videos
-  $('.trailer_play_button').hide();
-
-videoDiv.after(document.createElement('hr'));
-
-
-
-}
-
-function appendSelectionTo(parent, selection){
-  selection.each(function(){
-    parent.append(this);
-  });
-}
-
-function cleanUpEmptyParagraphs(){
-  jQuery('p').each(function() {
-      var $this = jQuery(this);
-      if($this.html().replace(/\s|&nbsp;/g, '').length == 0)
-          $this.remove();
-  });
 }
