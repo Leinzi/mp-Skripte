7dc8b80db6521d49338f14e591e922ab420fa484
Florian Leinsinger <leinzi1337@gmail.com>
2018-10-06 17:14:15 +0200

    remove "Relevante Links"

--- mp-no-relevant-links.user.js
+++ mp-no-relevant-links.user.js
@@ -5,16 +5,13 @@
 // @grant         none
 // @downloadURL   https://raw.githubusercontent.com/Leinzi/mp-Skripte/master/mp-no-relevant-links.user.js
 // @require       https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
-// @include       /\A(https?:\/\/www\.moviepilot.de\/news)(\?page=([1-9][0-9]*))?\z/
-// @version       0.1.2
+// @include       /^(https?:\/\/www\.moviepilot.de\/news)(\?page=([1-9][0-9]*))?$/
+// @version       0.1.3
 // ==/UserScript==
 
-var regWithoutSuffix        = /\A(https?:\/\/www\.moviepilot.de\/news)\z/;
-var regLatterPages          = /\A(https?:\/\/www\.moviepilot.de\/news)(\?page=([1-9][0-9]*))?\z/;
-
-var pages;
 
 // Funktion, damit das Dokument erst fertig geladen wird
 $(document).ready(function(){
-
-}
+  var linkParagraphs = $('p:contains("Relevante Links")');
+  linkParagraphs.remove();
+});
