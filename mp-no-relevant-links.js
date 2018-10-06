94e92d9d127cd1ba28e403d39b0143dfc0d94e91
Florian Leinsinger <leinzi1337@gmail.com>
2018-10-06 16:48:07 +0200

    Add new script

---
+++ mp-no-relevant-links.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name          mp-no-relevant-links
+// @author        leinzi
+// @description   Bilderstrecken auf Moviepilot umgehen
+// @grant         none
+// @downloadURL   https://raw.githubusercontent.com/Leinzi/mp-Skripte/master/mp-avoid-clickgal.user.js
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
+// @include       /^(https?:\/\/www\.moviepilot.de\/news\/)(.*?)$/
+// @version       0.1.0
+// ==/UserScript==
+
+var regWithoutSuffix        = /^(https?:\/\/www\.moviepilot.de\/news\/)([^\/\#]*?)$/;
+var regFirstPageOne         = /^(https?:\/\/www\.moviepilot.de\/news\/)([^"]*?)\/(seite-1)$/;
+var regLatterPages          = /^(https?:\/\/www\.moviepilot.de\/news\/)([^"]*?)\/(seite-([2-9]|2[0-6]))$/;
+
+// gibt es nicht mehr?
+var regWithCommentSuffix    = /^(https?:\/\/www\.moviepilot.de\/news\/)([^"]*?)\#(comments)$/;
+
+var pages;
+
+// Funktion, damit das Dokument erst fertig geladen wird
+$(document).ready(function(){
+
+}
