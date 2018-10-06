29bbca79229be132353020c138516c49919fbd5d
Florian Leinsinger <leinzi1337@gmail.com>
2018-10-06 16:54:26 +0200

    Rename script

--- mp-no-relevant-links.user.js
+++ mp-no-relevant-links.user.js
@@ -3,7 +3,7 @@
 // @author        leinzi
 // @description   Bilderstrecken auf Moviepilot umgehen
 // @grant         none
-// @downloadURL   https://raw.githubusercontent.com/Leinzi/mp-Skripte/master/mp-avoid-clickgal.user.js
+// @downloadURL   https://raw.githubusercontent.com/Leinzi/mp-Skripte/master/mp-no-relevant-links.user.js
 // @require       https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @include       /^(https?:\/\/www\.moviepilot.de\/news\/)(.*?)$/
 // @version       0.1.0
