c41e8e458d6b295b05405152a36b75c4fd7f745b
Leinzi <leinzi1337@gmail.com>
2021-12-28 20:48:55 +0100

    MPListExporter - Fix

--- mp-lists-csv-exporter.user.js
+++ mp-lists-csv-exporter.user.js
@@ -5,7 +5,7 @@
 // @grant               none
 // @downloadURL         https://github.com/Leinzi/mp-Skripte/raw/master/mp-lists-csv-exporter.user.js
 // @include             /^https?:\/\/www\.moviepilot.de\/liste\/([^\/\#]*?)
-// @version             0.1.1
+// @version             0.1.2
 // ==/UserScript==
 
 const LIST_REGEXP = /^https?:\/\/www\.moviepilot.de\/liste\/([^\/\#]+)/
